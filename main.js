8f152e838065ccaaa3f9659d22740365c550f4a7
Mary Dawn Malida <157961578+PseuDawncode@users.noreply.github.com>
2025-03-17 23:29:52 +0100

    inital commit

---
+++ main.js
@@ -0,0 +1,25 @@
+console.log('Hello World!');
+
+let name = "Mary Dawn";  
+let age = 27;
+let favoriteColor = "Emerald Green";
+
+console.log(`Name: ${name}, Age: ${age}, Favorite Color: ${favoriteColor}`);
+
+let houses = 48;
+houses = houses + 5;
+console.log(`The total of houses are: ${houses}`);
+
+let apples = 10;
+apples = apples - 1;
+console.log(`Mary gave 1 apple to David. How many apples left to Mary? ${apples} apples`);
+
+let x = 15;
+let y = 4;
+let a = x * y;
+console.log(`a is equals to ${a}`);
+
+let cats = 8;
+let playpen = 2;
+let cuteness = cats / playpen;
+console.log(`I have 8 cats and have 2 playpen. How many cats in each playpen? ${cuteness} cats`);
